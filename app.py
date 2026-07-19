63ae8156080e1a97c1389a8a0faf0ed92456e64f
sohanurislamtohin33-spec <sohanurislamtohin33@gmail.com>
2026-07-19 11:31:59 +0600

    Update app.py

--- app.py
+++ app.py
@@ -115,7 +115,7 @@ fetch_thread.start()
 # --- রাউটস ও এপিআই এন্ডপয়েন্টসমূহ ---
 @app.route('/')
 def home():
-    return render_template('dashboard.html) # অথবা আপনার মেইন ইনডেক্স/হোম পেজের নাম (যেমন: index.html)
+    return render_template('dashboard.html')
 @app.route('/console')
 def console_page():
     return render_template('console.html')
